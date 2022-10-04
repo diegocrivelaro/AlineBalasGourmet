489f6dea75d6212ec746afadd5f61b1b471cb35b
diegocrivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2022-10-04 17:03:15 -0300

    build: add jest and test library
    #13

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+/* module.exports = {
+  testEnvironment: `jsdom`,
+  preset: `ts-jest`,
+  setupFilesAfterEnv: [`<rootDir>/jest/setupTest.js`],
+  testPathIgnorePatterns: [`<rootDir>/.next`, `<rootDir>/node_modules/`],
+  moduleFileExtensions: [`js`, `jsx`, `ts`, `tsx`],
+  moduleDirectories: [`node_modules`, `<rootDir>`, `<rootDir>/src/`],
+  transformIgnorePatterns: [`node_modules`],
+  transform: {
+    "^.+\\.(js|ts|tsx)$": `babel-jest`,
+  },
+  moduleNameMapper: {
+    "^@/public/(.*)$": `<rootDir>/public/$1`,
+    "^@/src/(.*)$": `<rootDir>/src/$1`,
+    "^@/src/pages/(.*)$": `<rootDir>/src/pages/$1`,
+  },
+};
+ */
---
+++ src/test/index.test.tsx
@@ -0,0 +1,11 @@
+/* import Home from "@/pages/index";
+import { render, screen } from "@testing-library/react";
+
+describe(`2`, () => {
+  it(`2`, () => {
+    render(<Home />);
+
+    expect(screen.getByTestId(`casa"`)).toBeInTheDocument();
+  });
+});
+ */
