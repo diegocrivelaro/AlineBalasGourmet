05b66bac3693ae2e14f85567224c2d0507fd8191
diegocrivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2022-10-14 18:38:13 -0300

    chore(pages): update global meta tags
    #23

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,10 @@
-import Document, { Html, Head, Main, NextScript, DocumentProps } from "next/document";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentProps,
+} from "next/document";
 
 export default class MyDocument extends Document<DocumentProps> {
   render(): JSX.Element {
@@ -6,13 +12,16 @@ export default class MyDocument extends Document<DocumentProps> {
       <Html lang="pt-BR">
         <Head>
           <meta charSet="UTF-8" />
+          <meta httpEquiv="x-ua-compatible" content="IE=edge,chrome=1" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
 
         <body>
           <noscript>
             <p>
               <strong>
-                Você precisa habilitar o JavaScript do navegador para esse website funcionar.
+                Você precisa habilitar o JavaScript do navegador para esse
+                website funcionar.
               </strong>
             </p>
           </noscript>
