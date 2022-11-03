66840be9870b63e545117b1951ad50dbd30ceaed
diegocrivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2022-11-03 00:28:05 -0300

    feat(components): create DefaultLogo component
    #28

---
+++ src/components/DefaultLogo/index.tsx
@@ -0,0 +1,14 @@
+import DefaultLink from "@/components/DefaultLink";
+import { companyName } from "@/static/constants";
+
+import styles from "./styles.module.css";
+
+export default function DefaultLogo() {
+  return (
+    <DefaultLink
+      href="/"
+      titleAccessibility="Clique aqui para ir na página inicial">
+      <p className={styles.defaultLogo}>{companyName}</p>
+    </DefaultLink>
+  );
+}
