7a33ee6718ce905f5d1d4b276ea06437cd06dcd8
diegocrivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2022-11-04 11:22:07 -0300

    feat(static): add constant to menu links
    #28

--- src/static/constants.ts
+++ src/static/constants.ts
@@ -1,2 +1,35 @@
 export const companyName = `Aline Balas Gourmet`;
 export const companyNameNoSpaces = `AlineBalasGourmet`;
+
+export const headerNavLinks = [
+  {
+    name: `Home`,
+    path: `/`,
+    title: `Clique aqui para ir na página inicial`,
+  },
+  {
+    name: `Cardápio`,
+    path: `/cardapio`,
+    title: `Clique aqui para ir na página cardápio online`,
+  },
+  {
+    name: `Fotos`,
+    path: `/fotos`,
+    title: `Clique aqui para ir na página fotos`,
+  },
+  {
+    name: `Depoimentos`,
+    path: `/#depoimentos`,
+    title: `Clique aqui para ir seção depoimentos`,
+  },
+  {
+    name: `Sobre nós`,
+    path: `/#sobre-nos`,
+    title: `Clique aqui para ir seção sobre nós`,
+  },
+  {
+    name: `Contato`,
+    path: `/#contato`,
+    title: `Clique aqui para ir seção contato`,
+  },
+];
