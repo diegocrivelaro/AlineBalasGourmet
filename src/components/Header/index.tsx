3e6057c6a2d6278090d0c86fea8cabba21066e48
diegocrivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2022-11-09 16:22:03 -0300

    feat: big commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,28 @@
+import DefaultLink from "@/components/DefaultLink";
+import DefaultLogo from "@/components/DefaultLogo";
+import { headerNavLinks } from "@/static/constants";
+
+import styles from "./styles.module.css";
+
+export default function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <DefaultLogo />
+
+      <nav>
+        <ol className={styles.headerMenu}>
+          {headerNavLinks.map(navigationLink => (
+            <li key={navigationLink.title}>
+              <DefaultLink
+                href={navigationLink.path}
+                titleAccessibility={navigationLink.title}
+                cnLink={styles.headerMenuLink}>
+                {navigationLink.name}
+              </DefaultLink>
+            </li>
+          ))}
+        </ol>
+      </nav>
+    </header>
+  );
+}
