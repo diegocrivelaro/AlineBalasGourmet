690d81a583ca3163faf3b8876988e140fbee1ba6
Diego Crivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2023-01-19 19:30:28 -0300

    refactor(component): split hamburguer menu component
    #28

---
+++ src/components/Header/MenuHamburguer/index.tsx
@@ -0,0 +1,56 @@
+import Image from "next/image";
+
+import React, { useState } from "react";
+
+import DefaultLink from "@/components/DefaultComponents/DefaultLink";
+import { headerNavLinks } from "@/static/constants";
+
+import styles from "./styles.module.css";
+
+function MenuHamburguer() {
+  const [menuHamburguer, setMenuHamburguer] = useState(false);
+
+  return menuHamburguer ? (
+    <section className={styles.menuMobileContainer}>
+      <div className={styles.menuMobileBackground}>
+        <div className={styles.menuMobileContent}>
+          <Image
+            src="/images/icons/closeIcon.svg"
+            width={34}
+            height={34}
+            alt="Fechar menu de navegação"
+            aria-label="Botão para fechar o menu de navegação"
+            title="Fechará o menu de navegação"
+            onClick={() => setMenuHamburguer(false)}
+          />
+
+          <ol className={styles.headerMenu}>
+            {headerNavLinks.map(navigationLink => (
+              <li key={navigationLink.title}>
+                <DefaultLink
+                  href={navigationLink.path}
+                  titleAccessibility={navigationLink.title}
+                  cnLink={styles.headerMenuLink}>
+                  {navigationLink.name}
+                </DefaultLink>
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    </section>
+  ) : (
+    <Image
+      src="/images/icons/menuHamburguerIcon.svg"
+      width={28}
+      height={28}
+      alt="Abrir menu de navegação"
+      aria-label="Botão para abrir o menu de navegação"
+      title="Abrirá o menu de navegação"
+      className={styles.headerIconMenuHamburguer}
+      onClick={() => setMenuHamburguer(true)}
+    />
+  );
+}
+
+export default React.memo(MenuHamburguer);
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,62 +1,21 @@
-import Image from "next/image";
+import React from "react";
 
-import React, { useState } from "react";
-
-import DefaultLink from "@/components/DefaultComponents/DefaultLink";
 import DefaultLogo from "@/components/DefaultComponents/DefaultLogo";
-import { headerNavLinks } from "@/static/constants";
+import useWindowSize from "@/hooks/useWindowSize";
 
+import MenuHamburguer from "./MenuHamburguer";
 import styles from "./styles.module.css";
 
 function Header() {
-  const [menuHamburguer, setMenuHamburguer] = useState(false);
+  const { width } = useWindowSize();
+  const isMobileOrTablet = width <= 425;
 
   return (
     <header className={styles.headerContainer}>
       <DefaultLogo />
 
       <nav className={styles.headerNavContainer}>
-        {menuHamburguer ? (
-          <section className={styles.menuMobileContainer}>
-            <div className={styles.menuMobileBackground}>
-              <div className={styles.menuMobileContent}>
-                <Image
-                  src="/images/icons/closeIcon.svg"
-                  width={34}
-                  height={34}
-                  alt="Fechar menu de navegação"
-                  aria-label="Botão para fechar o menu de navegação"
-                  title="Fechará o menu de navegação"
-                  onClick={() => setMenuHamburguer(false)}
-                />
-
-                <ol className={styles.headerMenu}>
-                  {headerNavLinks.map(navigationLink => (
-                    <li key={navigationLink.title}>
-                      <DefaultLink
-                        href={navigationLink.path}
-                        titleAccessibility={navigationLink.title}
-                        cnLink={styles.headerMenuLink}>
-                        {navigationLink.name}
-                      </DefaultLink>
-                    </li>
-                  ))}
-                </ol>
-              </div>
-            </div>
-          </section>
-        ) : (
-          <Image
-            src="/images/icons/menuHamburguerIcon.svg"
-            width={28}
-            height={28}
-            alt="Abrir menu de navegação"
-            aria-label="Botão para abrir o menu de navegação"
-            title="Abrirá o menu de navegação"
-            className={styles.headerIconMenuHamburguer}
-            onClick={() => setMenuHamburguer(true)}
-          />
-        )}
+        {isMobileOrTablet ? <MenuHamburguer /> : <h1>casa</h1>}
       </nav>
     </header>
   );
