b38dd1ce57ef33bbdbab299728827aea20ac661e
Diego Crivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2023-01-19 19:21:02 -0300

    feat(hook): add useWindowSize hook
    #28

---
+++ src/hooks/useWindowSize.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+import { useWindowSizeProps } from "@/types/useWindowSizeProps";
+
+export default function useWindowSize() {
+  const [size, setSize] = useState<useWindowSizeProps>({ width: 0, height: 0 });
+
+  useEffect(() => {
+    function updateSize() {
+      setSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    window.addEventListener(`resize`, updateSize);
+
+    updateSize();
+
+    return () => window.removeEventListener(`resize`, updateSize);
+  }, []);
+
+  return size;
+}
