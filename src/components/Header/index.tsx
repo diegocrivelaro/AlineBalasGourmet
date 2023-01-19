7d1181f8064390c33e0a9ff49e77da60cfd643d8
Diego Crivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2023-01-19 20:30:02 -0300

    feat(component): make the header responsive for tablets
    #28

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,38 @@
 import React from "react";
 
+import DefaultLink from "@/components/DefaultComponents/DefaultLink";
 import DefaultLogo from "@/components/DefaultComponents/DefaultLogo";
 import useWindowSize from "@/hooks/useWindowSize";
+import { headerNavLinks } from "@/static/constants";
 
 import MenuHamburguer from "./MenuHamburguer";
 import styles from "./styles.module.css";
 
 function Header() {
   const { width } = useWindowSize();
-  const isMobileOrTablet = width <= 425;
+  const isMobile = width <= 425;
 
   return (
     <header className={styles.headerContainer}>
-      <DefaultLogo />
+      <DefaultLogo defaultLogoModifier={styles.logo} />
 
       <nav className={styles.headerNavContainer}>
-        {isMobileOrTablet ? <MenuHamburguer /> : <h1>casa</h1>}
+        {isMobile ? (
+          <MenuHamburguer />
+        ) : (
+          <ol className={styles.headerMenu}>
+            {headerNavLinks.map(navigationLink => (
+              <li key={navigationLink.title}>
+                <DefaultLink
+                  href={navigationLink.path}
+                  titleAccessibility={navigationLink.title}
+                  cnLink={styles.headerMenuLink}>
+                  {navigationLink.name}
+                </DefaultLink>
+              </li>
+            ))}
+          </ol>
+        )}
       </nav>
     </header>
   );
