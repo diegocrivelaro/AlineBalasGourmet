fbb152c1f22e152a013444c0828d5a7eb43e14db
Diego Crivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2023-03-02 20:36:33 -0300

    test(components): fix tests
    #28

--- src/components/Header/MenuHamburguer/tests/MenuHamburguer.test.tsx
+++ src/components/Header/MenuHamburguer/tests/MenuHamburguer.test.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
-
 import { render, screen, cleanup } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import MenuHamburguer from "..";
 
@@ -13,8 +12,23 @@ describe(`MenuHamburguer`, () => {
     cleanup();
   });
 
+  describe(`Behavior`, () => {
+    test(`Check if menu is opening after user click on menu image`, async () => {
+      render(<MenuHamburguer />);
+
+      const getImageMenu = screen.getByRole(`img`);
+
+      await userEvent.click(getImageMenu);
+
+      expect(screen.getByRole(`img`)).toHaveAttribute(
+        `aria-describedby`,
+        `Botão para fechar o menu de navegação`,
+      );
+    });
+  });
+
   describe(`Imagem`, () => {
-    test(`There should be an altText on the image`, () => {
+    test(`Should contain an AltText on the image with the text "Abrir menu de navegação"`, () => {
       render(<MenuHamburguer />);
 
       expect(
@@ -22,17 +36,16 @@ describe(`MenuHamburguer`, () => {
       ).toBeInTheDocument();
     });
 
-    test(`There should be an arialLabel on the image`, () => {
+    test(`Should contain an AriaDescribedby on the image with the text "Botão para abrir o menu de navegação"`, () => {
       render(<MenuHamburguer />);
 
       expect(screen.getByRole(`img`)).toHaveAttribute(
-        `aria-label`,
+        `aria-describedby`,
         `Botão para abrir o menu de navegação`,
       );
     });
 
-    // TODO: Finalize the tests for the true condition of the component
-    /* test(`There should be a title on the image`, () => {
+    test(`Should contain a title as an image attribute with the following text "Abrirá o menu de navegação"`, () => {
       render(<MenuHamburguer />);
 
       expect(screen.getByRole(`img`)).toHaveAttribute(
@@ -41,30 +54,42 @@ describe(`MenuHamburguer`, () => {
       );
     });
 
-    test(`There should be an altText on the image`, () => {
+    test(`There should be an altText with the text "Fechar menu de navegação" after clicking the button to open the menu`, async () => {
       render(<MenuHamburguer />);
+
+      const getImageMenu = screen.getByRole(`img`);
+
+      await userEvent.click(getImageMenu);
 
       expect(
         screen.getByAltText(`Fechar menu de navegação`),
       ).toBeInTheDocument();
     });
 
-    test(`There should be an arialLabel on the image`, () => {
+    test(`There should be an ariaDescribedby with the text "Botão para fechar o menu de navegação" after clicking the button to open the menu`, async () => {
       render(<MenuHamburguer />);
 
+      const getImageMenu = screen.getByRole(`img`);
+
+      await userEvent.click(getImageMenu);
+
       expect(screen.getByRole(`img`)).toHaveAttribute(
-        `aria-label`,
-        `Botão para abrir o menu de navegação`,
+        `aria-describedby`,
+        `Botão para fechar o menu de navegação`,
       );
     });
 
-    test(`There should be a title on the image`, () => {
+    test(`Should have a title as an attribute with the text "Fechará o menu de navegação" after clicking on the button to open the menu`, async () => {
       render(<MenuHamburguer />);
+
+      const getImageMenu = screen.getByRole(`img`);
+
+      await userEvent.click(getImageMenu);
 
       expect(screen.getByRole(`img`)).toHaveAttribute(
         `title`,
-        `Abrirá o menu de navegação`,
+        `Fechará o menu de navegação`,
       );
-    }); */
+    });
   });
 });
