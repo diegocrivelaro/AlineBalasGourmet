f23c5e80b219be596c74472b3bb2da0b346247de
Diego Crivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2023-04-11 20:07:29 -0300

    feat: add order button
    Desenvolver a section principal #31

--- src/components/Sections/HeroHomePage/index.tsx
+++ src/components/Sections/HeroHomePage/index.tsx
@@ -1,20 +1,34 @@
 import { memo } from "react";
 
+import DefaultButton from "@/components/DefaultComponents/DefaultButton";
 import HeroSliderHomePage from "@/components/HeroSliderHomePage";
 
 import styles from "./styles.module.css";
 
 function HeroHomePage() {
+  const buttonStyles = {
+    containerStyle: styles.heroHomePageButtonContainer,
+    buttonStyle: styles.heroHomePageButton,
+    bulletStyle: styles.heroHomePageBullet,
+  };
+
   return (
     <section className={styles.heroHomePageContainer}>
       <HeroSliderHomePage />
-
       <article className={styles.heroHomePageContent}>
         <h1 className={styles.heroHomePageTitle}>Balas para adoçar seu dia!</h1>
         <p className={styles.heroHomePageDescription}>
           Balas feitas com todo amor
         </p>
       </article>
+
+      <DefaultButton
+        href="/cardapio"
+        titleAccessibility="Faça seu pedido agora!"
+        style={buttonStyles}
+        hasBullet>
+        Fazer pedido!
+      </DefaultButton>
     </section>
   );
 }
