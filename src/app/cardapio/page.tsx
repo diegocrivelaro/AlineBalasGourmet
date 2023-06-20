f49b9e44a80840b9b7723fa7f3675139d39a6bd9
Diego Crivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2023-06-20 19:57:38 -0300

    feat: create menu page
    Criar página de cardápio #75

---
+++ src/app/cardapio/page.tsx
@@ -0,0 +1,13 @@
+import { metadataMenu } from "@/static/metadata";
+
+export const metadata = {
+  ...metadataMenu,
+};
+
+export default function Cardapio() {
+  return (
+    <main>
+      <h1>Cardápio</h1>
+    </main>
+  );
+}
