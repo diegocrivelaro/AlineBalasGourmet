7123649b445b030f40cd7fd9d1b393a1dd7ccf6c
Diego Crivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2023-06-20 23:14:29 -0300

    chore: add MediasProps typing
    Criar página de cardápio #75

--- src/types/generic.ts
+++ src/types/generic.ts
@@ -1,8 +1,18 @@
-export interface widthHeightProps {
+export type MediasTagsProps = `thumbnail` | `hero`;
+
+export interface WidthHeightProps {
   width: number;
   height: number;
 }
 
-export interface IconProps extends widthHeightProps {
+export interface IconProps extends WidthHeightProps {
   style?: string;
+}
+
+export interface MediasProps {
+  url: string | undefined;
+  description: string | undefined;
+  altText: string | undefined;
+  title: string | undefined;
+  tags: MediasTagsProps[] | null;
 }
