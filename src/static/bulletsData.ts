f52bd872a6a49f7b790c23582bd83778d4b243cb
Diego Crivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2023-06-20 23:15:20 -0300

    chore: add bullet data
    Criar página de cardápio #75

---
+++ src/static/bulletsData.ts
@@ -0,0 +1,560 @@
+import { BulletsProps } from "@/types/bulletsData";
+
+export const bullets: BulletsProps[] = [];
+
+export const traditionalIcedBullets: BulletsProps = {
+  title: `Balas Geladas Tradicionais`,
+  description: `Balas de coco geladas tradicionais`,
+  flavors: [
+    {
+      name: `Amendoim`,
+      description: undefined,
+      price: 68,
+      medias: null,
+    },
+    {
+      name: `Café`,
+      description: undefined,
+      price: 65,
+      medias: null,
+    },
+    {
+      name: `Chocolate`,
+      description: undefined,
+      price: 65,
+      medias: null,
+    },
+    {
+      name: `Limão`,
+      description: undefined,
+      price: 65,
+      medias: null,
+    },
+    {
+      name: `Morango`,
+      description: undefined,
+      price: 65,
+      medias: null,
+    },
+    {
+      name: `Nozes`,
+      description: undefined,
+      price: 80,
+      medias: null,
+    },
+    {
+      name: `Tradicional`,
+      description: undefined,
+      price: 65,
+      medias: null,
+    },
+  ],
+  batters: null,
+  chocolates: null,
+  quantityOptions: [
+    {
+      type: `Receita inteira`,
+      quantity: 120,
+      price: undefined,
+    },
+    {
+      type: `Meia receita`,
+      quantity: 60,
+      price: 48,
+    },
+  ],
+  colorOptions: null,
+};
+
+export const traditionalDryBullets: BulletsProps = {
+  title: `Balas Sequinhas Tradicionais`,
+  description: `Balas de coco sequinhas tradicionais`,
+  flavors: [
+    {
+      name: `Amendoim`,
+      description: undefined,
+      price: 55,
+      medias: null,
+    },
+    {
+      name: `Café`,
+      description: undefined,
+      price: 55,
+      medias: null,
+    },
+    {
+      name: `Chocolate`,
+      description: undefined,
+      price: 55,
+      medias: null,
+    },
+    {
+      name: `Limão`,
+      description: undefined,
+      price: 55,
+      medias: null,
+    },
+    {
+      name: `Morango`,
+      description: undefined,
+      price: 55,
+      medias: null,
+    },
+    {
+      name: `Nozes`,
+      description: undefined,
+      price: 65,
+      medias: null,
+    },
+    {
+      name: `Tradicional`,
+      description: undefined,
+      price: 55,
+      medias: null,
+    },
+  ],
+  batters: null,
+  chocolates: null,
+  quantityOptions: [
+    {
+      type: `Receita inteira`,
+      quantity: 220,
+      price: undefined,
+    },
+    {
+      type: `Meia receita`,
+      quantity: 110,
+      price: 40,
+    },
+  ],
+  colorOptions: null,
+};
+
+export const stuffedIcedBullets: BulletsProps = {
+  title: `Balas Geladas Recheadas`,
+  description: `Balas de coco geladas recheadas`,
+  flavors: [
+    {
+      name: `Bicho de pé (Brigadeiro de Morango)`,
+      description: undefined,
+      price: 105,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro de cereja`,
+      description: undefined,
+      price: 105,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro branco`,
+      description: undefined,
+      price: 105,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro de beijinho`,
+      description: undefined,
+      price: 105,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro de cranberry com frutas cristalizadas`,
+      description: undefined,
+      price: 110,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro de leite ninho`,
+      description: undefined,
+      price: 105,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro de limão`,
+      description: undefined,
+      price: 105,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro de oreo`,
+      description: undefined,
+      price: 105,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro de pistache`,
+      description: undefined,
+      price: 110,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro tradicional`,
+      description: undefined,
+      price: 105,
+      medias: null,
+    },
+    {
+      name: `Camafeu (Brigadeiro de Nozes)`,
+      description: undefined,
+      price: 110,
+      medias: null,
+    },
+    {
+      name: `Doce de abacaxi`,
+      description: undefined,
+      price: 110,
+      medias: null,
+    },
+    {
+      name: `Doce de damasco`,
+      description: undefined,
+      price: 105,
+      medias: null,
+    },
+    {
+      name: `Goiabada`,
+      description: undefined,
+      price: 105,
+      medias: null,
+    },
+    {
+      name: `Nutella`,
+      description: undefined,
+      price: 105,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro Tradicional com Cerejas`,
+      description: undefined,
+      price: 110,
+      medias: null,
+    },
+    {
+      name: `Nutella com Amêndoas Laminadas`,
+      description: undefined,
+      price: 110,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro de Leite Ninho com Amêndoas Laminadas`,
+      description: undefined,
+      price: 110,
+      medias: null,
+    },
+  ],
+  batters: [
+    `Amendoim`,
+    `Café`,
+    `Chocolate`,
+    `Limão`,
+    `Morango`,
+    `Nozes`,
+    `Tradicional`,
+  ],
+  chocolates: null,
+  quantityOptions: [
+    {
+      type: `Receita inteira`,
+      quantity: 120,
+      price: undefined,
+    },
+    {
+      type: `Meia receita`,
+      quantity: 60,
+      price: 74,
+    },
+    {
+      type: `Receita inteira com 2 sabores diferentes`,
+      quantity: 120,
+      price: 120,
+    },
+  ],
+  colorOptions: null,
+};
+
+export const stuffedDryBullets: BulletsProps = {
+  title: `Balas Sequinhas Recheadas`,
+  description: `Balas de coco sequinhas recheadas`,
+  flavors: [
+    {
+      name: `Bicho de pé (Brigadeiro de Morango)`,
+      description: undefined,
+      price: 105,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro de cereja`,
+      description: undefined,
+      price: 105,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro branco`,
+      description: undefined,
+      price: 105,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro de beijinho`,
+      description: undefined,
+      price: 105,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro de cranberry com frutas cristalizadas`,
+      description: undefined,
+      price: 110,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro de leite ninho`,
+      description: undefined,
+      price: 105,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro de limão`,
+      description: undefined,
+      price: 105,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro de oreo`,
+      description: undefined,
+      price: 105,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro de pistache`,
+      description: undefined,
+      price: 110,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro tradicional`,
+      description: undefined,
+      price: 105,
+      medias: null,
+    },
+    {
+      name: `Camafeu (Brigadeiro de Nozes)`,
+      description: undefined,
+      price: 110,
+      medias: null,
+    },
+    {
+      name: `Doce de abacaxi`,
+      description: undefined,
+      price: 110,
+      medias: null,
+    },
+    {
+      name: `Doce de damasco`,
+      description: undefined,
+      price: 105,
+      medias: null,
+    },
+    {
+      name: `Goiabada`,
+      description: undefined,
+      price: 105,
+      medias: null,
+    },
+    {
+      name: `Nutella`,
+      description: undefined,
+      price: 105,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro Tradicional com Cerejas`,
+      description: undefined,
+      price: 110,
+      medias: null,
+    },
+    {
+      name: `Nutella com Amêndoas Laminadas`,
+      description: undefined,
+      price: 110,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro de Leite Ninho com Amêndoas Laminadas`,
+      description: undefined,
+      price: 110,
+      medias: null,
+    },
+  ],
+  batters: [
+    `Amendoim`,
+    `Café`,
+    `Chocolate`,
+    `Limão`,
+    `Morango`,
+    `Nozes`,
+    `Tradicional`,
+  ],
+  chocolates: null,
+  quantityOptions: [
+    {
+      type: `Receita inteira`,
+      quantity: 200,
+      price: undefined,
+    },
+    {
+      type: `Meia receita`,
+      quantity: 100,
+      price: 74,
+    },
+    {
+      type: `Receita inteira com 2 sabores diferentes`,
+      quantity: 200,
+      price: 120,
+    },
+  ],
+  colorOptions: null,
+};
+
+export const stuffedCandyBullets: BulletsProps = {
+  title: `Balas de Bombom Recheadas`,
+  description: `Balas de coco de bombom`,
+  flavors: [
+    {
+      name: `Bicho de pé (Brigadeiro de Morango)`,
+      description: undefined,
+      price: 140,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro de cereja`,
+      description: undefined,
+      price: 140,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro branco`,
+      description: undefined,
+      price: 140,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro de beijinho`,
+      description: undefined,
+      price: 140,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro de cranberry com frutas cristalizadas`,
+      description: undefined,
+      price: 140,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro de leite ninho`,
+      description: undefined,
+      price: 140,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro de limão`,
+      description: undefined,
+      price: 140,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro de oreo`,
+      description: undefined,
+      price: 140,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro de pistache`,
+      description: undefined,
+      price: 140,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro tradicional`,
+      description: undefined,
+      price: 140,
+      medias: null,
+    },
+    {
+      name: `Camafeu (Brigadeiro de Nozes)`,
+      description: undefined,
+      price: 140,
+      medias: null,
+    },
+    {
+      name: `Doce de abacaxi`,
+      description: undefined,
+      price: 140,
+      medias: null,
+    },
+    {
+      name: `Doce de damasco`,
+      description: undefined,
+      price: 140,
+      medias: null,
+    },
+    {
+      name: `Goiabada`,
+      description: undefined,
+      price: 140,
+      medias: null,
+    },
+    {
+      name: `Nutella`,
+      description: undefined,
+      price: 140,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro Tradicional com Cerejas`,
+      description: undefined,
+      price: 140,
+      medias: null,
+    },
+    {
+      name: `Nutella com Amêndoas Laminadas`,
+      description: undefined,
+      price: 140,
+      medias: null,
+    },
+    {
+      name: `Brigadeiro de Leite Ninho com Amêndoas Laminadas`,
+      description: undefined,
+      price: 140,
+      medias: null,
+    },
+  ],
+  batters: [
+    `Amendoim`,
+    `Café`,
+    `Chocolate`,
+    `Limão`,
+    `Morango`,
+    `Nozes`,
+    `Tradicional`,
+  ],
+  chocolates: null,
+  quantityOptions: [
+    {
+      type: `Receita inteira`,
+      quantity: 120,
+      price: undefined,
+    },
+    {
+      type: `Meia receita`,
+      quantity: 60,
+      price: 104,
+    },
+    {
+      type: `Receita inteira com 2 sabores diferentes`,
+      quantity: 120,
+      price: 150,
+    },
+  ],
+  colorOptions: null,
+};
