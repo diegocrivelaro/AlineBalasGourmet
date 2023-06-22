d6b4fd2c75fe38e5e4bb2862ba63fe421f2d3e1b
Diego Crivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2023-06-21 22:05:41 -0300

    feat: create component "SearchResultsList"
    Criar página de cardápio #75

---
+++ src/components/Sections/SearchResultsList/index.tsx
@@ -0,0 +1,30 @@
+import { memo } from "react";
+
+import CardBullet from "@/components/CardBullet";
+
+import { bullets } from "@/static/bulletsData";
+
+import styles from "./styles.module.css";
+
+function SearchResultsList() {
+  return (
+    <>
+      {bullets.map(bullet => (
+        <section key={bullet.id}>
+          <div className={styles.cards}>
+            {bullet.flavors.map(flavor => (
+              <CardBullet
+                flavorName={flavor.name}
+                price={flavor.price}
+                medias={flavor.medias}
+                key={flavor.id}
+              />
+            ))}
+          </div>
+        </section>
+      ))}
+    </>
+  );
+}
+
+export default memo(SearchResultsList);
