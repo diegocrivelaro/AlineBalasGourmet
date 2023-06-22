f207fddcd408fc6c55e0dcad82c332da8acbc187
Diego Crivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2023-06-22 10:23:54 -0300

    chore: add bullet title and quantity options
    Criar página de cardápio #75

--- src/components/Sections/SearchResultsList/index.tsx
+++ src/components/Sections/SearchResultsList/index.tsx
@@ -11,6 +11,18 @@ function SearchResultsList() {
     <>
       {bullets.map(bullet => (
         <section className={styles.container} key={bullet.id}>
+          <div className={styles.informationWrapper}>
+            <h3 className={styles.bulletName}>{bullet.title}</h3>
+
+            <ol className={styles.quantityOptionsWrapper}>
+              {bullet.quantityOptions.map(option => (
+                <li className={styles.amountOfBullets} key={option.id}>
+                  {`${option.type} (${option.mass}, com aprox. ${option.quantity})`}
+                </li>
+              ))}
+            </ol>
+          </div>
+
           <div className={styles.cards}>
             {bullet.flavors.map(flavor => (
               <CardBullet
