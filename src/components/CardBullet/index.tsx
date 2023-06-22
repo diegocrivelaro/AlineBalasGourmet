0444b7967c8962184fa910c6807cbd3e9166ee40
Diego Crivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2023-06-21 22:06:03 -0300

    feat: create component "CardBullet"
    Criar página de cardápio #75

---
+++ src/components/CardBullet/index.tsx
@@ -0,0 +1,34 @@
+import { memo } from "react";
+
+import { MediasProps } from "@/types/generic";
+
+import { formatCurrencyInReais } from "@/utils/helpers";
+
+import styles from "./styles.module.css";
+import ThumbnailCard from "./ThumbnailCard";
+
+interface CardBulletProps {
+  flavorName: string;
+  price: number;
+  medias: MediasProps[] | null;
+}
+
+function CardBullet({ flavorName, price, medias }: CardBulletProps) {
+  const getFormattedPrice = formatCurrencyInReais(price);
+
+  return (
+    <section className={styles.container}>
+      <div className={styles.informationWrapper}>
+        <p className={styles.flavorName}>{flavorName}</p>
+
+        <p className={styles.price}>{getFormattedPrice}</p>
+      </div>
+
+      <div className={styles.thumbnailWrapper}>
+        <ThumbnailCard medias={medias} />
+      </div>
+    </section>
+  );
+}
+
+export default memo(CardBullet);
