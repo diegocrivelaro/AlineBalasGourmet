fec8fad43e88a3ea7ac036c15e223fd8a6bff2f9
Diego Crivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2023-06-27 13:48:43 -0300

    chore: add divider between types of bullets
    Criar página de cardápio #75

--- src/components/Sections/SearchResultsList/index.tsx
+++ src/components/Sections/SearchResultsList/index.tsx
@@ -1,6 +1,7 @@
 import { memo } from "react";
 
 import CardBullet from "@/components/CardBullet";
+import Divider from "@/components/Divider";
 
 import { bullets } from "@/static/bulletsData";
 
@@ -10,44 +11,48 @@ function SearchResultsList() {
   return (
     <>
       {bullets.map(bullet => (
-        <section className={styles.container} key={bullet.id}>
-          <div className={styles.informationWrapper}>
-            <h3 className={styles.bulletName}>
-              {bullet.title}
-            </h3>
+        <>
+          <section className={styles.container} key={bullet.id}>
+            <div className={styles.informationWrapper}>
+              <h3 className={styles.bulletName}>
+                {bullet.title}
+              </h3>
 
-            <ol className={styles.quantityOptionsWrapper}>
-              {bullet.quantityOptions.map(option => (
-                <li className={styles.amountOfBullets} key={option.id}>
-                  {option.type}
-                  {` `}
-                  (
-                  <span className={styles.strong}>
-                    {option.mass}
-                  </span>
-                  , com aprox.
-                  {` `}
-                  <span className={styles.strong}>
-                    {option.quantity}
-                  </span>
-                  )
-                </li>
+              <ol className={styles.quantityOptionsWrapper}>
+                {bullet.quantityOptions.map(option => (
+                  <li className={styles.amountOfBullets} key={option.id}>
+                    {option.type}
+                    {` `}
+                    (
+                    <span className={styles.strong}>
+                      {option.mass}
+                    </span>
+                    , com aprox.
+                    {` `}
+                    <span className={styles.strong}>
+                      {option.quantity}
+                    </span>
+                    )
+                  </li>
+                ))}
+              </ol>
+            </div>
+
+            <div className={styles.cards}>
+              {bullet.flavors.map(flavor => (
+                <CardBullet
+                  bulletName={bullet.title}
+                  flavorName={flavor.name}
+                  price={flavor.price}
+                  medias={flavor.medias}
+                  key={flavor.id}
+                />
               ))}
-            </ol>
-          </div>
+            </div>
+          </section>
 
-          <div className={styles.cards}>
-            {bullet.flavors.map(flavor => (
-              <CardBullet
-                bulletName={bullet.title}
-                flavorName={flavor.name}
-                price={flavor.price}
-                medias={flavor.medias}
-                key={flavor.id}
-              />
-            ))}
-          </div>
-        </section>
+          <Divider />
+        </>
       ))}
     </>
   );
